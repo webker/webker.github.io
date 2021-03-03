b7609420b2705dc734b9f1e575e469f9164283bb
xufeng <pangpangxu@gmail.com>
2021-03-03 21:30:36 +0800

    Site updated: 2021-03-03 21:30:36

--- js/copyright.js
+++ js/copyright.js
@@ -1,1 +1,17 @@
-!function(t,s,e){var o,c=document.currentScript||(o=document.getElementsByTagName("script"))[o.length-1],n=$(c).attr("successtext");new ClipboardJS(".fa-clipboard").on("success",function(){n&&(toastr.options={positionClass:"toast-top-center",timeOut:"1000"},toastr.success(n))})}(window,document);
+!function (e, t, a) {
+    var script = document.currentScript || (function () {
+        var scripts = document.getElementsByTagName("script");
+        return scripts[scripts.length - 1]
+    })()
+    var successText = $(script).attr("successtext")
+    var clipboard = new ClipboardJS('.fa-clipboard');
+    clipboard.on('success', function () {
+        if (successText) {
+            toastr.options = {
+                "positionClass": "toast-top-center",
+                "timeOut": "1000",
+            }
+            toastr.success(successText)
+        }
+    });
+}(window, document);
